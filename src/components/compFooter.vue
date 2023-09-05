e28b0184bcb2fe0c89b36d5dbdbf7cbba58e7e65
phanhiep <phanhiep@example.com>
2023-09-05 15:29:55 +0700

    giao dien food-detail

--- src/components/compFooter.vue
+++ src/components/compFooter.vue
@@ -2,115 +2,123 @@
   <div class="footer">
     <div class="container">
       <div class="row">
-        <div class="col">
-          <div class="footer__inner">
-            <div class="footer__content">
-              <div class="footer__left">
-                <img src="../assets/icon/logo.svg" alt="" />
-                <p class="desc footer__left-desc">
-                  Food, drinks, groceries, and more available for delivery and
-                  pickup.
-                </p>
-                <div class="footer__social">
-                  <a class="footer__social-link">
-                    <img src="../assets/img/facebook.svg" alt="" />
-                  </a>
-                  <a class="footer__social-link">
-                    <img src="../assets/img/in.svg" alt=""
-                  /></a>
-                  <a class="footer__social-link">
-                    <img src="../assets/img/instagram.svg" alt=""
-                  /></a>
-                  <a class="footer__social-link">
-                    <img src="../assets/img/twitter.svg" alt=""
-                  /></a>
-                </div>
-              </div>
-              <div class="footer__right">
-                <!-- list 1 -->
-                <ul class="footer__list">
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Get Started</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">FoodEats Sign In</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">FoodEats Sign Up</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Become a Rider</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Become a Partner</a>
-                  </li>
-                </ul>
-                <!-- list 2 -->
-                <ul class="footer__list">
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Get Help</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Resources</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Support</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Contact Us</a>
-                  </li>
-                </ul>
-                <!-- list 3 -->
-                <ul class="footer__list">
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Company</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">About Us</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Customer Rights</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Career</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Press</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Blog</a>
-                  </li>
-                </ul>
-                <!-- list 4 -->
-                <ul class="footer__list">
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">FootEats For</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Enterprise</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">For Small Business</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Personal</a>
-                  </li>
-                  <li class="footer__item">
-                    <a href="" class="footer__item-link">Riders</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="footer__span"></div>
-            <div class="footer__copyright">
-              <p class="footer__copyright-desc">
-                Copyright © 2022 UIHUT all rights reserved.
+        <div class="col-lg-3 col-sm-12">
+          <div class="footer__top-left">
+            <div class="footer__left">
+              <img src="../assets/icon/logo.svg" alt="" />
+              <p class="desc footer__left-desc">
+                Food, drinks, groceries, and more available for delivery and
+                pickup.
               </p>
-              <div class="footer__copyright-sub">
-                <p>Privacy Policy</p>
-                <p>Terms & Conditions</p>
+              <div class="footer__social">
+                <a class="footer__social-link">
+                  <img src="../assets/img/facebook.svg" alt="" />
+                </a>
+                <a class="footer__social-link">
+                  <img src="../assets/img/in.svg" alt=""
+                /></a>
+                <a class="footer__social-link">
+                  <img src="../assets/img/instagram.svg" alt=""
+                /></a>
+                <a class="footer__social-link">
+                  <img src="../assets/img/twitter.svg" alt=""
+                /></a>
               </div>
             </div>
+          </div>
+        </div>
+        <div class="col-lg-2 offset-lg-1 col-sm-12">
+          <div class="footer__top-right">
+            <ul class="footer__list">
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Get Started</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">FoodEats Sign In</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">FoodEats Sign Up</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Become a Rider</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Become a Partner</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 col-sm-12">
+          <div class="footer__top-right">
+            <ul class="footer__list">
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Get Started</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">FoodEats Sign In</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">FoodEats Sign Up</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Become a Rider</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Become a Partner</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 col-sm-12">
+          <div class="footer__top-right">
+            <ul class="footer__list">
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Get Started</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">FoodEats Sign In</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">FoodEats Sign Up</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Become a Rider</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Become a Partner</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-2 col-sm-12">
+          <div class="footer__top-right">
+            <ul class="footer__list">
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Get Started</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">FoodEats Sign In</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">FoodEats Sign Up</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Become a Rider</a>
+              </li>
+              <li class="footer__item">
+                <a href="" class="footer__item-link">Become a Partner</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer__span"></div>
+        <div class="footer__copyright">
+          <p class="footer__copyright-desc">
+            Copyright © 2022 UIHUT all rights reserved.
+          </p>
+          <div class="footer__copyright-sub">
+            <p>Privacy Policy</p>
+            <p>Terms & Conditions</p>
           </div>
         </div>
       </div>
--- src/pages/pagesFoodDetail.vue
+++ src/pages/pagesFoodDetail.vue
@@ -1,12 +1,51 @@
 <template>
   <section>
     <div class="container">
+      <a href="" class="back__food">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="6"
+          height="10"
+          viewBox="0 0 6 10"
+          fill="none"
+        >
+          <path
+            d="M5 9L1 5L5 1"
+            stroke="#191720"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        <h4 class="back__food-title">Back to The Burger Cafe</h4>
+      </a>
       <div class="row">
         <div class="col-lg-6 col-sm-12">
-          <div class="inner">left</div>
+          <div class="food__detail-left">
+            <img
+              src="../assets/img/detail-burger-01.png"
+              alt=""
+              class="food__detail-img"
+            />
+          </div>
         </div>
         <div class="col-lg-5 offset-lg-1 col-sm-12">
-          <div class="inner">right</div>
+          <div class="food__detail-right">
+            <h3 class="food__detail-title">Eggs Benedict Burger</h3>
+            <p class="food__detail-desc">
+              Ground beef, hollandaise sauce mix, stone ground mustard, with a
+              egg it’s so much taste you would like most this taste burger.
+            </p>
+            <h5 class="food__detail-price">$7.50</h5>
+            <div class="food__detail-quantity">
+              <div class="input-number">
+                <el-input-number controls v-model="num" :min="1" :max="10" />
+              </div>
+              <button class="food__detail-quantity-btn">
+                Add to cart: <span>15.00</span>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -14,7 +53,80 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      num: 1,
+    };
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.back__food {
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+}
+.back__food-title {
+  color: var(--Dark, #191720);
+  font-size: 1.8rem;
+  font-weight: 600;
+  line-height: 1.667;
+  padding-left: 5px;
+}
+.food__detail-img {
+  width: 570px;
+  height: 570px;
+}
+.food__detail-left {
+  padding-bottom: 160px;
+}
+.food__detail-title {
+  color: var(--Dark, #191720);
+  font-size: 3rem;
+  font-weight: 700;
+  line-height: 1.33;
+}
+.food__detail-desc {
+  width: 470px;
+  margin-top: 30px;
+  color: var(--Grey, #575363);
+  font-size: 1.8rem;
+  font-weight: 400;
+  line-height: 1.667;
+}
+.food__detail-price {
+  margin-top: 18px;
+  color: var(--Orange, #f05e36);
+  font-size: 2rem;
+  font-weight: 700;
+  line-height: 1.5;
+}
+.food__detail-quantity {
+  display: flex;
+  align-items: center;
+  margin-top: 50px;
+  column-gap: 18px;
+}
+.el-input-number {
+  position: relative;
+  display: inline-flex;
+  width: 172px;
+  height: 60px;
+  line-height: 30px;
+}
+.food__detail-quantity-btn {
+  width: 280px;
+  height: 60px;
+  flex-shrink: 0;
+  border-radius: 6px;
+  background: var(--Dark, #191720);
+  border: none;
+  color: #fff;
+  cursor: pointer;
+}
+.food__detail-quantity-btn:hover {
+  opacity: 0.9;
+}
+</style>
