53e7a3a17129f7ea1e439c1e022fefd83e03502b
phanhiep <phanhiep@example.com>
2023-09-06 23:06:08 +0700

    blog-post

--- src/pages/pagesBlogPost.vue
+++ src/pages/pagesBlogPost.vue
@@ -1,13 +1,55 @@
 <template>
-  <section>
+  <section class="blog-post">
     <div class="container">
       <div class="row">
         <div class="col-lg-6 col-sm-12">
-          <div class="inner">left</div>
+          <div class="blog__top">
+            <div class="blog__top-left">
+              <div class="blog__top-read">
+                <p class="blog__top-time desc">Nove 06, 2022</p>
+                <span></span>
+                <p class="blog__top-time desc">14 min read</p>
+              </div>
+              <h2 class="blog__top-title">
+                Most popular vegetarian and vegan restaurants in your city.
+              </h2>
+              <p class="desc blog__top-desc">
+                Looking to add more leafy greens and plant-based burgers to your
+                plate this fall? Today, in celebration of World Vegetarian Day
+                on October 1, we discover few restaurants for you.
+              </p>
+              <div class="blog__top-info">
+                <img
+                  src="../assets/img/blog-avatar.png"
+                  alt=""
+                  class="blog__top-info-img"
+                />
+                <p class="blog__top-info-name">David Warner</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="col-lg-5 offset-lg-1 col-sm-12">
-          <div class="inner">right</div>
+          <div class="blog__top-right">
+            <img
+              src="../assets/img/blog-img.png"
+              alt=""
+              class="blog__top-img"
+            />
+          </div>
         </div>
+      </div>
+      <div class="blog-post__social">
+        <a class="blog-post__social-link">
+          <img src="../assets/img/facebook.svg" alt="" />
+        </a>
+
+        <a class="blog-post__social-link">
+          <img src="../assets/img/instagram.svg" alt=""
+        /></a>
+        <a class="blog-post__social-link">
+          <img src="../assets/img/twitter.svg" alt=""
+        /></a>
       </div>
     </div>
   </section>
@@ -16,23 +58,155 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 offset-lg-2 col-sm-12">
-          <div class="inner">center</div>
+          <div class="blog-post__content">
+            <h4 class="blog-post__content-title">The Royal Dine</h4>
+            <p class="desc blog-post__content-desc">
+              Children, especially younger children, can be influenced by how
+              the food looks. Fun shapes and bright colors may grab their
+              attention and encourage them to take a bite. You don’t have to
+              spend a lot of time carving cartoon characters out of bologna.
+              Just take a few minutes to arrange the lunch neatly, add a
+              personal note or use a cookie cutter to shape a sandwich or slice
+              of cheese to add visual interest to the meal. “If the food looks
+              fun and appetizing to you, there is a good chance your picky eater
+              will also find it appetizing,” Thivierge says.
+            </p>
+            <img
+              src="../assets/img/blog-post01.png"
+              alt=""
+              class="blog-post__content-img"
+            />
+          </div>
+        </div>
+        <div class="col-lg-8 offset-lg-2 col-sm-12">
+          <div class="blog-post__content">
+            <h4 class="blog-post__content-title">The Dish Express</h4>
+            <p class="desc blog-post__content-desc">
+              Children, especially younger children, can be influenced by how
+              the food looks. Fun shapes and bright colors may grab their
+              attention and encourage them to take a bite. You don’t have to
+              spend a lot of time carving cartoon characters out of bologna.
+              Just take a few minutes to arrange the lunch neatly, add a
+              personal note or use a cookie cutter to shape a sandwich or slice
+              of cheese to add visual interest to the meal. “If the food looks
+              fun and appetizing to you, there is a good chance your picky eater
+              will also find it appetizing,” Thivierge says.
+            </p>
+            <img
+              src="../assets/img/blog-post02.png"
+              alt=""
+              class="blog-post__content-img"
+            />
+          </div>
         </div>
       </div>
     </div>
   </section>
   <!-- section 3 -->
-  <section>
+  <section class="blog-post-footer">
     <div class="container">
+      <div class="blog-post__reading">
+        <h2 class="blog-post__reading-title">Read similar blog</h2>
+      </div>
       <div class="row">
         <div class="col-lg-4 col-sm-12">
-          <div class="inner">left</div>
+          <div class="blog__content">
+            <ul class="blog__list">
+              <li class="blog__item">
+                <img
+                  src="../assets/img/blog-img03.png"
+                  alt=""
+                  class="blog__item-img"
+                />
+                <div class="blog__item-time">
+                  <p class="desc">Nove 06, 2022</p>
+                  <span></span>
+                  <p class="desc">14 min read</p>
+                </div>
+                <h5 class="blog__item-title">
+                  Top 10 vegetarian and vegan restaurants in your city.
+                </h5>
+                <p class="desc blog__item-desc">
+                  Looking to add more leafy greens and plant-based burgers to
+                  your plate this fall? Today, in celebration of World...
+                </p>
+                <div class="blog__item-info">
+                  <img
+                    src="../assets/img/blog-avatar.png"
+                    alt=""
+                    class="blog__item-avatar"
+                  />
+                  <p class="blog__item-name">David Warner</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
         <div class="col-lg-4 col-sm-12">
-          <div class="inner">center</div>
+          <div class="blog__content">
+            <ul class="blog__list">
+              <li class="blog__item">
+                <img
+                  src="../assets/img/blog-img03.png"
+                  alt=""
+                  class="blog__item-img"
+                />
+                <div class="blog__item-time">
+                  <p class="desc">Nove 06, 2022</p>
+                  <span></span>
+                  <p class="desc">14 min read</p>
+                </div>
+                <h5 class="blog__item-title">
+                  Top 10 vegetarian and vegan restaurants in your city.
+                </h5>
+                <p class="desc blog__item-desc">
+                  Looking to add more leafy greens and plant-based burgers to
+                  your plate this fall? Today, in celebration of World...
+                </p>
+                <div class="blog__item-info">
+                  <img
+                    src="../assets/img/blog-avatar.png"
+                    alt=""
+                    class="blog__item-avatar"
+                  />
+                  <p class="blog__item-name">David Warner</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
         <div class="col-lg-4 col-sm-12">
-          <div class="inner">right</div>
+          <div class="blog__content">
+            <ul class="blog__list">
+              <li class="blog__item">
+                <img
+                  src="../assets/img/blog-img03.png"
+                  alt=""
+                  class="blog__item-img"
+                />
+                <div class="blog__item-time">
+                  <p class="desc">Nove 06, 2022</p>
+                  <span></span>
+                  <p class="desc">14 min read</p>
+                </div>
+                <h5 class="blog__item-title">
+                  Top 10 vegetarian and vegan restaurants in your city.
+                </h5>
+                <p class="desc blog__item-desc">
+                  Looking to add more leafy greens and plant-based burgers to
+                  your plate this fall? Today, in celebration of World...
+                </p>
+                <div class="blog__item-info">
+                  <img
+                    src="../assets/img/blog-avatar.png"
+                    alt=""
+                    class="blog__item-avatar"
+                  />
+                  <p class="blog__item-name">David Warner</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -43,4 +217,50 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.blog-post {
+  margin-top: 55px;
+}
+.blog-post__social {
+  display: flex;
+  column-gap: 15px;
+  margin-top: 30px;
+  justify-content: center;
+}
+.blog-post__content {
+  margin-top: 55px;
+  margin-bottom: 160px;
+}
+.blog-post__content-title {
+  color: var(--Dark, #191720);
+  font-size: 2.6rem;
+  font-weight: 600;
+  line-height: 1.38;
+}
+.blog-post__content-desc {
+  margin-top: 18px;
+  width: 770px;
+}
+.blog-post__content-img {
+  margin-top: 55px;
+  width: 770px;
+  height: 470px;
+  flex-shrink: 0;
+  object-fit: cover;
+  border-radius: 12px;
+}
+.blog-post__reading {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 55px;
+}
+.blog-post__reading-title {
+  color: var(--Dark, #191720);
+  font-size: 4.4rem;
+  font-weight: 600;
+  line-height: 1;
+}
+.blog-post-footer {
+  margin-bottom: 160px;
+}
+</style>
